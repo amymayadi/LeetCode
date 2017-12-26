7f03a2b2e9a94adc1e44ee7d43fd34b54ce4b3e2
Yadi Ma <amymayadi@msn.com>
2017-12-26 14:58:28 +0800

    modify compile error

--- C++/E088MergeSortedArray/MergeTwoSortedArrayCH.cpp
+++ C++/E088MergeSortedArray/MergeTwoSortedArrayCH.cpp
@@ -2,15 +2,15 @@
 // Author : Hao Chen
 // Date   : 2014-06-20
 
-/********************************************************************************** 
-* 
+/**********************************************************************************
+*
 * Given two sorted integer arrays A and B, merge B into A as one sorted array.
-* 
+*
 * Note:
-*   You may assume that A has enough space (size that is greater or equal to m + n) 
-*   to hold additional elements from B. The number of elements initialized in A and B 
+*   You may assume that A has enough space (size that is greater or equal to m + n)
+*   to hold additional elements from B. The number of elements initialized in A and B
 *   are m and n respectively.
-*               
+*
 **********************************************************************************/
 
 #include <stdio.h>
@@ -18,16 +18,16 @@
 void merge(int A[], int m, int B[], int n) {
     int ia = m-1 ;
     int ib = n-1 ;
-    int i = m + n - 1;
+    //int i = m + n - 1;
     for (int i=m+n-1; i>=0; i--){
-        
+
         if (ia>=0 && ib<0){
             break;
         }
         if (ia<0 && ib>=0){
             A[i] = B[ib--];
             continue;
-        }            
+        }
         if (ia>=0 && ib>=0){
             if (A[ia] > B[ib]){
                 A[i] = A[ia--];
