9e9881b5f617ed8f43fe4b8813c0f194a58a69b6
amymayadi <ma.athenadianan@gmai.com>
2018-04-05 13:51:22 +0800

    E026RemoveDuplicatesFromSortedArray.cpp idea written by mayadi

---
+++ C++/E026RemoveDuplicates/E026RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int n = nums.size();
+        if(n==0)
+            return 0;
+        int j = 1; //待存位置
+        int key=nums[0];
+        for(int i=1; i<n; i++){
+            if(key != nums[i]){ //未判断数组起点
+                key = nums[i];
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+        return j;
+    }
+};
