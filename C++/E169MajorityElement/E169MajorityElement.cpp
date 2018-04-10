a1ce36dc6158a512b9bae4fe6dac670ba509e06d
马雅狄 <mayadi@travelsky.com>
2018-04-10 15:09:23 +0800

    E169MajorityElement.cpp -- Hash Table

---
+++ C++/E169MajorityElement/E169MajorityElement.cpp
@@ -0,0 +1,67 @@
+/**
+ *  Hash Table -- unordered_map
+ *  time:O(n)
+ *  space:O(n)
+ */
+#include <iostream>
+#include <vector>
+#include <sstream>
+#include <include/c++/algorithm>
+#include <include/c++/unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        unordered_map<int, int> hash;
+        const int n = nums.size();
+        for (const int num : nums) {
+            // put num into hash, its index is count.
+	    // hash[num] default set to be 0;
+            if(++hash[num] > n/2)
+                return num;
+        }
+        return -1;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> nums = stringToIntegerVector(line);
+
+        int ret = Solution().majorityElement(nums);
+
+        string out = to_string(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
